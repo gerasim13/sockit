000f16afcf633b3461515806893dfbf8ed624a63
Павел Литвиненко <gerasim13@gmail.com>
2015-01-15 22:02:20 +0300

    Update of .gitignore file.

--- build/SOCKit.build/Debug-iphoneos/SOCKit.build/Objects-normal/armv7/SOCKit.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pavellitvinenko/Documents/Projects/Telesite/Thirdparty/iOS/sockit/SOCKit.m \
-  /Users/pavellitvinenko/Documents/Projects/Telesite/Thirdparty/iOS/sockit/SOCKit.h
